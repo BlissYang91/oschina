9ef87d00209e1cf41564b6183108832ca5a29053
BlissYang91 <2812420513@qq.com>
2019-10-15 22:12:22 +0800

    变量

--- lib/test/demo.dart
+++ lib/test/demo.dart
@@ -2,6 +2,29 @@ import 'package:flutter/material.dart';
 void main(){
   var val = 12 ~/ 7;
   print(val);
+
+  void printItem(String item){
+    print(item);
+  }
+
+  // 将方法printItem作为参数传递，遍历数组中的item，传入方法中
+  var users = ['小明','xiaowang','xiaozhang'];
+  users.forEach(printItem);
+
+  //函数赋值给变量
+  var say = (name){
+    print(name);
+  };
+  say('下班了');
+  /**
+   * 结果：
+   * flutter: 1
+      flutter: 小明
+      flutter: xiaowang
+      flutter: xiaozhang
+      flutter: 下班了
+
+   */
   runApp(
     Center(
       child: Text(
